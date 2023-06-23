dbce0f22026a1c5dfe29b2665bff8b8bcaee0f65
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2023-06-23 11:01:44 -0300

    Sacada ruta que no se usa

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ use App\Http\Controllers\UserController;
 Route::prefix('v1')->group(function ()
 {
     Route::post('/user',[UserController::class,"Register"]);
-    Route::post('/login',[UserController::class,"Login"]);
     Route::get('/validate',[UserController::class,"ValidateToken"])->middleware('auth:api');
     Route::get('/logout',[UserController::class,"Logout"])->middleware('auth:api');
 
