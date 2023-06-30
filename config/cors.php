6274a2a01842a4337034edc749d5541446292646
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2023-06-30 12:11:47 -0300

    Ajustado cors

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'oauth/token'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://superred.com'],
 
     'allowed_origins_patterns' => [],
 
