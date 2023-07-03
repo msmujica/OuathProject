6902248e1f4c0464acbbe2d87ebca0a085c07373
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2023-07-03 14:12:59 -0300

    Agregadas pruebas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Laravel\Passport\Client;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +19,17 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(1)->create([
             "name" => "usuario",
             "email" => "usuario@email.com"
+        ]);
+
+        Client::create([
+            'id' => 100,
+            'name' => 'Tests',
+            'secret' => "wsBa0mp4jwSTYssUGHX5xoqD9IC0X95Gfpg0w3uY",
+            'redirect' => 'http://localhost',
+            'provider' => 'users',
+            'personal_access_client' => false,
+            'password_client' => true,
+            'revoked' => false
         ]);
     }
 }
